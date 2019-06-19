3247de5b613e1c787c158a58f9580e5e37124027
Sarah Lopez <Sarahanne.pez@gmail>
2019-06-18 21:49:38 -0700

    updated to latest

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-$("button").on("click", function() {
-    const keyword = $(this).attr("data-keyword");
-    
-    const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?="
-     + keyword + "&api-key=infs8sNRCHkd40EWJIGLYHjPiWft0q9L"
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        
-        
-    })
-    
-})
 
-console.log(keyword)
+$("button").on("click", function() {
+    //search term var
+    const keyword = $("#search-term").val()
+    //start date var
+    let startDate = $("#begin-date").val()
+    
+    //end date var
+    let endDate = $("#blah").val()
+    //attr("data-keyword");
+    console.log(keyword);
+    const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&begin_date=" + startDate + "&end_date=" + endDate + "&api-key=infs8sNRCHkd40EWJIGLYHjPiWft0q9L"
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+console.log(response);
+});
+});
+console.log("hi");
+
